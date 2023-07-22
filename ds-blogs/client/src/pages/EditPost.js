ab3e20b1f8c2b6a199f5c1f61d66015e9d378ed9
Durgesh Suthar <100849645+DurgeshSuthar@users.noreply.github.com>
2023-07-22 12:46:33 +0530

    Update EditPost.js

--- ds-blogs/client/src/pages/EditPost.js
+++ ds-blogs/client/src/pages/EditPost.js
@@ -12,7 +12,7 @@ export default function EditPost() {
     const [authFail, setAuthFail] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:4000/post/'+id).then(response => {
+        fetch('https://ds-blogs-server.vercel.app/post/'+id).then(response => {
             response.json().then(postInfo => {
                 setTitle(postInfo.title);
                 setContent(postInfo.content);
@@ -30,7 +30,7 @@ export default function EditPost() {
         if(files?.[0]){
             data.set('file', files?.[0]);
         }
-        const response = await fetch('http://localhost:4000/post' ,{
+        const response = await fetch('https://ds-blogs-server.vercel.app/post' ,{
             method: 'PUT',
             body: data,
             credentials: 'include',
