8fe66a38da3d77c15f0b1b68b588eed323cb771d
Durgesh Suthar <100849645+DurgeshSuthar@users.noreply.github.com>
2023-07-23 12:19:04 +0530

    Update Header.js

--- ds-blogs/client/src/Header.js
+++ ds-blogs/client/src/Header.js
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 export default function Header() {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch(`/profile`, {
+    fetch(`https://ds-blogs-api.onrender.com/profile`, {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch('/logout', {
+    fetch('https://ds-blogs-api.onrender.com/logout', {
       credentials: 'include',
       method: 'POST',
     });
