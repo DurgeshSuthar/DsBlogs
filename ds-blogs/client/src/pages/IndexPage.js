1993de946650e7e278b0ce3e66c53a722b3a8f83
Durgesh Suthar <100849645+DurgeshSuthar@users.noreply.github.com>
2023-07-26 19:45:09 +0530

    Update IndexPage.js

--- ds-blogs/client/src/pages/IndexPage.js
+++ ds-blogs/client/src/pages/IndexPage.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Post from "../Post";
+import loading from "../loading.gif";
 
 export default function IndexPage() {
     const [posts, setPosts] = useState([]);
+    const [load, setLoad] = useState(true);
     useEffect(() => {
         fetch('https://ds-blogs-api.onrender.com/post').then(response => {
             response.json().then(posts => {
@@ -11,8 +13,19 @@ export default function IndexPage() {
         });
     }, []);
     return (
-        <>
-            {posts.length > 0 && posts.map(post => (
+       <>
+            {load && (
+                <>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className="spin">
+                      <img src={loading} alt="Loading..."/>
+                </div>
+              </>
+            )}
+            {!load && posts.length > 0 && posts.map(post => (
                 <Post {...post} />
             ))}
         </>
